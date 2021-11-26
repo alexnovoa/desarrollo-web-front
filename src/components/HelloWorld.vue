e962e32be97dca46fa14e5f3d2d46dfb102a12fc
Alex <alex.novoa.r@gmail.com>
2021-11-26 09:49:12 -0500

    Se realizaron los ejercicios de la guia 9

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
+  <div class="container">
     {{nombre}}
       <ul>
         <li v-for="item in clientes" :key="item.id">
             {{item.nombre}}
+        </li>
+      </ul>
+      <input type="text" v-model="nombre">
+      <br>
+      <a v-bind:href="url">{{url}}</a>
+      <p/>
+      <button @click='metodo1'>Ejecutar metodo</button>
+      <p></p>
+
+      <h1>Ejercicio 1</h1>
+      <p></p>
+      <input type="number" v-model="numero1">
+      <input type="number" v-model="numero2">
+      <p></p>
+      <button @click="sumar">Sumar +</button>
+      <p></p>
+      <h3>Resultado {{resultado}}</h3>
+
+      <p></p>
+      <h1>Ejercicio 2</h1>
+      <input type="text" v-model="tarea"/>
+     
+      <button @click="agregarTarea">Agregar tarea</button>
+      <h5>Listado de tareas</h5>
+       <ul>
+        <li v-for="item in tareas" :key="item">
+            {{item}}
         </li>
       </ul>
   </div> 
@@ -19,12 +46,25 @@ export default {
     return {
       nombre : 'Alex',
       mostrar : true,
-      clientes : [{id:1, nombre : 'Alex'},{id:2, nombre : 'Luis'}]
+      clientes : [{id:1, nombre : 'Alex'},{id:2, nombre : 'Luis'}],
+      url : 'https://www.google.com',
+      numero1 : 0,
+      numero2 : 0,
+      resultado : 0,
+      tarea : null,
+      tareas : ['descripcion']
     }
   },
   methods : {
     metodo1 () {
-        console.log('Ingresa al metodo')
+        alert("Click")
+    },
+    sumar () {
+      this.resultado = this.numero1+this.numero2
+    },
+    agregarTarea () {
+      alert("agregar tarea "+ this.tarea)
+      this.tareas.push(this.tarea)
     }
   }
 }
